38756f9f6e0c5c6a328fafab1e68a89bf16c5bff
Morgan York <me@myork.net>
2024-09-17 16:43:02 -0400

    Update optum.js to Lead position

--- src/data/optum.js
+++ src/data/optum.js
@@ -1,9 +1,10 @@
 export default {
   name: "Optum",
-  position: "Senior Software Engineer",
+  position: "Lead Software Engineer",
   timeframe: "June 2021 - Present",
   location: "Remote",
   bullets: [
+    "Led development on a new in-house visualization tool combining data from numerous sources, reducing the lift for any team in Optum wanting to share visualizations with a broad internal audience",
     "Delivered a new custom content-management tool for an internal training platform using Next.js, increasing administrator productivity through detailed user interviews",
     "Enabled the development of new content-management tool through automatic GraphQL API generation, based on defined record types",
     "Adopted an engineer-facing training application, collaborating with business stakeholders, users, and other engineers in an agile fashion to improve site functionality and the user experience",
